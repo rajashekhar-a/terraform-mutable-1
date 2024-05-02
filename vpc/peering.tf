63fcff19e1c9b9b9e78baf457fb56a7bcb1ce984
rajashekhar-a <rajuavunuri123@gmail.com>
2024-05-02 14:26:03 +0530

    update changes

--- vpc/peering.tf
+++ vpc/peering.tf
@@ -1,8 +1,8 @@
-resource "aws_vpc_peering_connection" "foo" {
+resource "aws_vpc_peering_connection" "peer" {
   peer_vpc_id   = aws_vpc.main.id
   vpc_id        = "var.DEFAULT_VPC_ID"
 
   tags = {
-    Name = "${ENV}-vpc-to-default-vpc"
+    Name = "${var.ENV}-vpc-to-default-vpc"
   }
 }
