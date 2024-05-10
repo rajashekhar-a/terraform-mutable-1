c909a5bf9c92f0bd701238da3272823acba4ef11
rajashekhar-a <rajuavunuri123@gmail.com>
2024-05-10 18:55:27 +0530

    updaye the changes for route table

--- vpc/routes.tf
+++ vpc/routes.tf
@@ -5,11 +5,37 @@ resource "aws_route_table" "private-route" {
     {
       cidr_block                = "var.DEFAULT_VPC_CIDR"
       vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+      cidr_block                   = var.DEFAULT_VPC_CIDR
+      vpc_peering_connection_id    = aws_vpc_peering_connection.peer.id
+      carrier_gateway_id           = ""
+      "destination_prefix_list_id" = ""
+      "egress_only_gateway_id"     = ""
+      "gateway_id"                 = ""
+      "instance_id"                = ""
+      "ipv6_cidr_block"            = ""
+      "local_gateway_id"           = ""
+      "nat_gateway_id"             = ""
+      "network_interface_id"       = ""
+      "transit_gateway_id"         = ""
+      "vpc_endpoint_id"            = ""
     },
 
     {
       cidr_block       = "0.0.0.0/0"
       "nat_gateway_id" = aws_nat_gateway.ngw.id
+      cidr_block                   = var.DEFAULT_VPC_CIDR
+      vpc_peering_connection_id    = aws_vpc_peering_connection.peer.id
+      carrier_gateway_id           = ""
+      "destination_prefix_list_id" = ""
+      "egress_only_gateway_id"     = ""
+      "gateway_id"                 = ""
+      "instance_id"                = ""
+      "ipv6_cidr_block"            = ""
+      "local_gateway_id"           = ""
+      "nat_gateway_id"             = ""
+      "network_interface_id"       = ""
+      "transit_gateway_id"         = ""
+      "vpc_endpoint_id"            = ""
     }
 
 
@@ -27,10 +53,36 @@ resource "aws_route_table" "pubilc-route" {
     {
       cidr_block                = "var.DEFAULT_VPC_CIDR"
       vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+      cidr_block                   = var.DEFAULT_VPC_CIDR
+      vpc_peering_connection_id    = aws_vpc_peering_connection.peer.id
+      carrier_gateway_id           = ""
+      "destination_prefix_list_id" = ""
+      "egress_only_gateway_id"     = ""
+      "gateway_id"                 = ""
+      "instance_id"                = ""
+      "ipv6_cidr_block"            = ""
+      "local_gateway_id"           = ""
+      "nat_gateway_id"             = ""
+      "network_interface_id"       = ""
+      "transit_gateway_id"         = ""
+      "vpc_endpoint_id"            = ""
     },
     {
       cidr_block   = "0.0.0.0/0"
       "gateway_id" = aws_internet_gateway.igw.id
+      cidr_block                   = var.DEFAULT_VPC_CIDR
+      vpc_peering_connection_id    = aws_vpc_peering_connection.peer.id
+      carrier_gateway_id           = ""
+      "destination_prefix_list_id" = ""
+      "egress_only_gateway_id"     = ""
+      "gateway_id"                 = ""
+      "instance_id"                = ""
+      "ipv6_cidr_block"            = ""
+      "local_gateway_id"           = ""
+      "nat_gateway_id"             = ""
+      "network_interface_id"       = ""
+      "transit_gateway_id"         = ""
+      "vpc_endpoint_id"            = ""
     }
 
 
