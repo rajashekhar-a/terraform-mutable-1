5bf93690095029067ee9ce1ee8913125e3cebb6e
rajashekhar-a <rajuavunuri123@gmail.com>
2024-05-11 12:11:36 +0530

    update changes

--- DB/data.tf
+++ DB/data.tf
@@ -1,12 +1,11 @@
 data "terraform_remote_state" "vpc" {
   backend = "s3"
-
   config = {
     bucket = "terraform-b60"
     key    = "terraform-mutable/vpc/${var.env}/terraform.tfstate"
     region = "us-east-1"
-    }
   }
+}
 
 data "aws_secretsmanager_secret" "secrets" {
   name = var.env
