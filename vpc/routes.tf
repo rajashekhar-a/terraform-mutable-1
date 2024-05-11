4edece748a886d29bbb4c0f6d5b93e44b4db6dbd
rajashekhar-a <rajuavunuri123@gmail.com>
2024-05-11 09:05:45 +0530

    upadte changes

--- vpc/routes.tf
+++ vpc/routes.tf
@@ -1,40 +1,40 @@
 resource "aws_route_table" "private-route" {
   vpc_id = aws_vpc.main.id
 
-  route = [
-    {
-      cidr_block                   = "172.31.0.0/16"
-      vpc_peering_connection_id    = aws_vpc_peering_connection.peer.id
-      carrier_gateway_id           = ""
-      "destination_prefix_list_id" = ""
-      "egress_only_gateway_id"     = ""
-      "gateway_id"                 = ""
-      "instance_id"                = ""
-      "ipv6_cidr_block"            = ""
-      "local_gateway_id"           = ""
-      "nat_gateway_id"             = ""
-      "network_interface_id"       = ""
-      "transit_gateway_id"         = ""
-      "vpc_endpoint_id"            = ""
-      "core_network_arn"           = ""
-    },
-    {
-      cidr_block                   = "0.0.0.0/0"
-      vpc_peering_connection_id    = ""
-      carrier_gateway_id           = ""
-      "destination_prefix_list_id" = ""
-      "egress_only_gateway_id"     = ""
-      "gateway_id"                 = ""
-      "instance_id"                = ""
-      "ipv6_cidr_block"            = ""
-      "local_gateway_id"           = ""
-      "nat_gateway_id"             = aws_nat_gateway.ngw.id
-      "network_interface_id"       = ""
-      "transit_gateway_id"         = ""
-      "vpc_endpoint_id"            = ""
-      "core_network_arn"           = ""
-    }
-  ]
+#  route = [
+#    {
+#      cidr_block                   = "172.31.0.0/16"
+#      vpc_peering_connection_id    = aws_vpc_peering_connection.peer.id
+#      carrier_gateway_id           = ""
+#      "destination_prefix_list_id" = ""
+#      "egress_only_gateway_id"     = ""
+#      "gateway_id"                 = ""
+#      "instance_id"                = ""
+#      "ipv6_cidr_block"            = ""
+#      "local_gateway_id"           = ""
+#      "nat_gateway_id"             = ""
+#      "network_interface_id"       = ""
+#      "transit_gateway_id"         = ""
+#      "vpc_endpoint_id"            = ""
+#      "core_network_arn"           = ""
+#    },
+#    {
+#      cidr_block                   = "0.0.0.0/0"
+#      vpc_peering_connection_id    = ""
+#      carrier_gateway_id           = ""
+#      "destination_prefix_list_id" = ""
+#      "egress_only_gateway_id"     = ""
+#      "gateway_id"                 = ""
+#      "instance_id"                = ""
+#      "ipv6_cidr_block"            = ""
+#      "local_gateway_id"           = ""
+#      "nat_gateway_id"             = aws_nat_gateway.ngw.id
+#      "network_interface_id"       = ""
+#      "transit_gateway_id"         = ""
+#      "vpc_endpoint_id"            = ""
+#      "core_network_arn"           = ""
+#    }
+#  ]
 
   tags = {
     Name = "private-route"
