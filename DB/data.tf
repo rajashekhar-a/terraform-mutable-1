97e8c1f00b3f8398bf5c7ff4bd813ca769ebe87b
rajashekhar-a <rajuavunuri123@gmail.com>
2024-05-11 12:21:43 +0530

    update state backend

--- DB/data.tf
+++ DB/data.tf
@@ -1,8 +1,8 @@
 data "terraform_remote_state" "vpc" {
   backend = "s3"
   config = {
-    bucket = "terraform-b60"
-    key    = "terraform-mutable/vpc/${var.env}/terraform.tfstate"
+    bucket = "terraform-state-backend1"
+    key    = "terraform-mutable/vpc/dev/terraform.tfstate"
     region = "us-east-1"
   }
 }
