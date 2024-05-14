70cb4398471e10ed08536aedc393e37b792f15a7
rajashekhar-a <rajuavunuri123@gmail.com>
2024-05-14 16:06:37 +0530

    setup the application load balancer

---
+++ ALB/env/prod-backend.tfvars
@@ -0,0 +1,3 @@
+bucket = "terraform-state-backend1"
+key    = "terraform-mutable/alb/dev/terraform.tfstate"
+region = "us-east-1"
---
+++ ALB/public-alb.tf
@@ -0,0 +1,13 @@
+resource "aws_lb" "public" {
+  name               = "roboshop-public-${var.ENV}"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.public-alb.id]
+  subnets            = data.terraform_remote_state.vpc.outputs.PUBLIC_SUBNETS_IDS
+
+  enable_deletion_protection = false
+
+  tags = {
+    Environment = "roboshop-public-${var.ENV}"
+  }
+}
